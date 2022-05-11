f3d85a9bd0e2647c06cb1d3c0eff88e06d39e231
vitaly <wrather28@gmail.com>
2022-05-11 17:12:07 +0300

    changed income model

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,7 @@
 class Income < ApplicationRecord
   belongs_to :user
+
+  scope :with_user, ->(user_id) {where('user_id = ?', user_id)}
+  scope :with_predefined, ->{where(predefined: true)}
+  
 end
