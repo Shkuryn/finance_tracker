6c26a2da7b89dea7b09e6f23f5994411bae107b8
Igor <shkuryn@gmail.com>
2022-05-15 00:01:51 +0400

    add other test for expence model

--- spec/models/expence_spec.rb
+++ spec/models/expence_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Expence, type: :model do
 
   describe '#create' do
     context 'successfully' do
-      it 'is valid with name, description' do
+      it 'is valid when all fields is filfull' do
         expect(@expence).to be_valid
       end
     end
@@ -19,8 +19,26 @@ RSpec.describe Expence, type: :model do
         @expence.name = nil
         expect(@expence).not_to be_valid
       end
+      it 'is invalid when name is too short' do
+        @expence.name = 'IS'
+        expect(@expence).not_to be_valid
+      end
+      it 'is invalid when name is too long' do
+        name = ''
+        51.times do
+          name += 'a'
+        end
+        @expence.name = name
+        expect(@expence).not_to be_valid
+      end
 
-      it 'is invalid without content' do
+      it 'is invalid without user_id' do
+        @expence.user_id = nil
+        expect(@expence).not_to be_valid
+      end
+      it 'is invalid without correct user_id' do
+        @expence.user_id = '999'
+        expect(@expence).not_to be_valid
       end
     end
   end
