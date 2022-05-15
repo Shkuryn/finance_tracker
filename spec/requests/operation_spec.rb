d0f9ca1c14bab1bbea965384a974c1a944846f30
Igor <shkuryn@gmail.com>
2022-05-15 15:54:19 +0400

    add show test for operation_controller

--- spec/requests/operation_spec.rb
+++ spec/requests/operation_spec.rb
@@ -2,12 +2,25 @@
 
 require 'rails_helper'
 
-RSpec.describe  OperationsController, type: :controller do
-  # let(:user) { FactoryBot.create :user }
+RSpec.describe OperationsController, type: :controller do
+  before do
+    @user = FactoryBot.create(:user)
+    @operaton = FactoryBot.create(:operation, user_id: @user.id)
+  end
+
   describe 'GET index' do
     it 'returns a 200' do
       get :index
       expect(response).to have_http_status(:ok)
     end
+  end
+  describe 'GET show' do
+    it 'to #show' do
+      get :show, params: { id: @operaton }
+    end
+  end
+  after do
+    @user.destroy
+    @operaton.destroy
   end
 end
