fdeec79982edea0382ee86427f9783f3e1916f57
Igor <shkuryn@gmail.com>
2022-05-15 10:03:27 +0400

    add welcome-controller_spec.rb

--- spec/controllers/welcome-controller_spec.rb
+++ spec/controllers/welcome-controller_spec.rb
@@ -4,4 +4,10 @@ require 'rails_helper'
 
 # Change this ArticlesController to your project
 RSpec.describe WelcomeController, type: :controller do
+  describe 'GET index' do
+    it 'returns a 200' do
+      get :index
+      expect(response).to have_http_status(:ok)
+    end
+  end
 end
