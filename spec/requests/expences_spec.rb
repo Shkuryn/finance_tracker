574340ebea998f054ce2ec82f063ce39b8b8e968
Igor <shkuryn@gmail.com>
2022-05-15 15:48:53 +0400

    add show test for expences_controller

--- spec/requests/expences_spec.rb
+++ spec/requests/expences_spec.rb
@@ -3,11 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe ExpencesController, type: :controller do
-  let(:user) { FactoryBot.create :user }
+  before do
+    @user = FactoryBot.create(:user)
+    @expence = FactoryBot.create(:expence, user_id: @user.id)
+  end
   describe 'GET index' do
     it 'returns a 200' do
       get :index
       expect(response).to have_http_status(:ok)
     end
+    describe 'GET show' do
+      it 'to #show' do
+        get :show, params: { id: @expence }
+      end
+    end
+  end
+  after do
+    @user.destroy
+    @expence.destroy
   end
 end
