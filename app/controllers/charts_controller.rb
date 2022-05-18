8a18980a4cd403757bc562479b55f53971a7dc28
Igor <shkuryn@gmail.com>
2022-05-18 23:46:19 +0400

    make charts sorted

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -3,6 +3,6 @@
 class ChartsController < ApplicationController
   def show
     @data = OperationDetail.joins('INNER JOIN expences on expences.id =operation_details.expence_id')
-                           .group(:name).sum(:amount)
+                           .group(:name).sum(:amount).sort_by { |_key, value| value }.reverse.to_h
   end
 end
