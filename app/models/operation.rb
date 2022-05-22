e68271d5af2d094d2943fa3945ef9b58aed19819
Igor <shkuryn@gmail.com>
2022-05-23 00:11:16 +0400

    add some beauty for index operation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -5,4 +5,9 @@ class Operation < ApplicationRecord
             inclusion: { in: 5.years.ago..5.years.from_now }
   has_many :operation_details, dependent: :destroy
   scope :with_user, ->(user_id) { where('user_id = ?', user_id) }
+  def user_presentation
+    name = "Vas"
+    surname ="Pup"
+    "#{name} #{surname}"
+  end
 end
