73999eebba5d1d4bba6efd5306200bc4e302ceb1
Igor <shkuryn@gmail.com>
2022-05-23 21:46:12 +0400

    add some beauty to expences/index & apply rubocop

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,7 +2,7 @@
 
 class Income < ApplicationRecord
   belongs_to :user
-  self.primary_key = "id"
+  self.primary_key = 'id'
 
   scope :with_user, ->(user_id) { where('user_id = ?', user_id) }
   scope :with_predefined, -> { where(predefined: true) }
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_511_142_620) do
+ActiveRecord::Schema.define(version: 20_220_517_155_714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -42,30 +42,14 @@ ActiveRecord::Schema.define(version: 20_220_511_142_620) do
     t.index ['reset_password_token'], name: 'index_admin_users_on_reset_password_token', unique: true
   end
 
-  create_table 'operation_details', force: :cascade do |t|
-    t.decimal 'amount'
-    t.integer 'operation_id'
-    t.integer 'expence_id'
-    t.string 'comment'
+  create_table 'expences', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.boolean 'predefined'
+    t.bigint 'user_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
-  end
-
-  create_table 'operations', force: :cascade do |t|
-    t.string 'comment'
-    t.boolean 'marked'
-    t.datetime 'date'
-    t.integer 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-  create_table "expences", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.boolean "predefined", default: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_expences_on_user_id"
+    t.index ['user_id'], name: 'index_expences_on_user_id'
   end
 
   create_table 'incomes', id: false, force: :cascade do |t|
