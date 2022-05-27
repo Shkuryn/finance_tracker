48dde78fa69156efef4fdafa4b93e2eb30dca9ef
vitaly <wrather28@gmail.com>
2022-05-27 23:19:40 +0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,9 +9,11 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2022_05_17_155714) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -28,7 +28,20 @@ ActiveRecord::Schema.define(version: 2022_05_17_155714) do
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
   end
-create_table "expences", force: :cascade do |t|
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "expences", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.boolean "predefined", default: false
@@ -38,50 +51,54 @@ create_table "expences", force: :cascade do |t|
     t.index ["user_id"], name: "index_expences_on_user_id"
   end
 
-  create_table 'incomes', force: :cascade do |t|
-    t.string 'name'
-    t.string 'description'
-    t.boolean 'predefined'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_incomes_on_user_id'
+  create_table "incomes", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.boolean "predefined"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_incomes_on_user_id"
   end
 
-  create_table 'operation_details', force: :cascade do |t|
-    t.decimal 'amount'
-    t.integer 'operation_id'
-    t.integer 'expence_id'
-    t.string 'comment'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "operation_details", force: :cascade do |t|
+    t.decimal "amount"
+    t.string "comment"
+    t.bigint "expences_id", null: false
+    t.bigint "operations_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["expences_id"], name: "index_operation_details_on_expences_id"
+    t.index ["operations_id"], name: "index_operation_details_on_operations_id"
   end
 
-  create_table 'operations', force: :cascade do |t|
-    t.string 'comment'
-    t.boolean 'marked'
-    t.datetime 'date'
-    t.integer 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "operations", force: :cascade do |t|
+    t.string "comment"
+    t.boolean "marked"
+    t.datetime "date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'name'
-    t.string 'surname'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "surname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'expences', 'users'
-  add_foreign_key 'incomes', 'users'
-  add_foreign_key 'operation_details', 'expences'
-  add_foreign_key 'operation_details', 'operations'
+  add_foreign_key "expences", "users"
+  add_foreign_key "incomes", "users"
+  add_foreign_key "operation_details", "expences", column: "expences_id"
+  add_foreign_key "operation_details", "operations", column: "operations_id"
+  add_foreign_key "operations", "users"
 end
