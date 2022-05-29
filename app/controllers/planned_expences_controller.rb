cc2e506012446ff9b69cfd6eda0feb3d66fbe199
vitaly <wrather28@gmail.com>
2022-05-29 23:44:40 +0300

    create views for planned expences with simle form

--- app/controllers/planned_expences_controller.rb
+++ app/controllers/planned_expences_controller.rb
@@ -1,4 +1,9 @@
-class PlannedExpencesController < ApplicationController
-  def index
-  end
+class PlannedExpencesController < InheritedResources::Base
+
+  private
+
+    def planned_expence_params
+      params.require(:planned_expence).permit(:expence_id, :description, :date, :sent, :user_id, :amount)
+    end
+
 end
--- spec/views/planned_expences/show.html.erb_spec.rb
+++ spec/views/planned_expences/show.html.erb_spec.rb
@@ -2,10 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "planned_expences/show", type: :view do
   before(:each) do
-    @planned_expence = assign(:planned_expence, PlannedExpence.create!())
+    @planned_expence = assign(:planned_expence, PlannedExpence.create!(
+      expence: nil,
+      description: "Description",
+      sent: false,
+      user: nil,
+      amount: 2
+    ))
   end
 
   it "renders attributes in <p>" do
     render
+    expect(rendered).to match(//)
+    expect(rendered).to match(/Description/)
+    expect(rendered).to match(/false/)
+    expect(rendered).to match(//)
+    expect(rendered).to match(/2/)
   end
 end
