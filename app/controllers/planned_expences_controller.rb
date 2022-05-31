af36574613d5d2f7a17759de7fabff4aa7e1defa
Vitaly <wrather28@gmail.com>
2022-05-31 17:18:53 +0300

    add update and destroy methods for planned expences controller

--- app/controllers/planned_expences_controller.rb
+++ app/controllers/planned_expences_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PlannedExpencesController < ApplicationController
+  before_action :set_planned_expence, only: %i[show edit update destroy]
+
   def index
     @planned_expences = PlannedExpence.where(Expence.with_user(current_user.id))
   end
@@ -36,11 +38,34 @@ class PlannedExpencesController < ApplicationController
     end
   end
 
-  
+  def update
+    respond_to do |format|
+      if @planned_expence.update(planned_expence_params)
+        format.html { redirect_to planned_expence_url(@planned_expence), notice: "Planned Expence was successfully updated." }
+        format.json { render :show, status: :ok, location: @planned_expence }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @eplanned_xpence.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @planned_expence.destroy
+
+    respond_to do |format|
+      format.html { redirect_to planned_expences_url, notice: "Planned Expence was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
   private
 
   def planned_expence_params
     params.require(:planned_expence).permit(:expence_id, :description, :date, :sent, :user_id, :amount, :name)
+  end
+
+  def set_planned_expence
+    @planned_expence = PlannedExpence.find(params[:id])
   end
 end
