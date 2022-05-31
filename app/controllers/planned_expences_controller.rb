42da4d11b420ef510e64f7f96d08291f41017d89
Vitaly <wrather28@gmail.com>
2022-05-31 17:02:18 +0300

    add create method for planned expences controller

--- app/controllers/planned_expences_controller.rb
+++ app/controllers/planned_expences_controller.rb
@@ -1,25 +1,46 @@
 # frozen_string_literal: true
 
 class PlannedExpencesController < ApplicationController
-  def index; end
+  def index
+    @planned_expences = PlannedExpence.where(Expence.with_user(current_user.id))
+  end
 
   def new
     @planned_expence = PlannedExpence.new
+    @expences = Expence.where(predefined: true).or(Expence.with_user(current_user.id))
   end
 
   def edit
     @planned_expence = PlannedExpence.find_by(id: params[:id])
   end
 
+  def show
+    @planned_expence = PlannedExpence.find(params[:id])
+    @expences = Expence.where(predefined: true).or(Expence.with_user(current_user.id))
+  end
+
   def create
-    @planned_expence = PlannedExpence.new
+    @expences = Expence.where(predefined: true).or(Expence.with_user(current_user.id))
+    @planned_expence = PlannedExpence.new(planned_expence_params)
     @planned_expence.user_id = current_user.id
     @planned_expence.sent = false
+
+    respond_to do |format|
+      if @planned_expence.save
+        format.html { redirect_to planned_expence_url(@planned_expence), notice: "Planned Expence was successfully created." }
+        format.json { render :show, status: :created, location: @planned_expence }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @planned_expence.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  
 
   private
 
   def planned_expence_params
-    params.require(:planned_expence).permit(:expence_id, :description, :date, :sent, :user_id, :amount)
+    params.require(:planned_expence).permit(:expence_id, :description, :date, :sent, :user_id, :amount, :name)
   end
 end
