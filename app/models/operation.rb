532bfa8dc994bf0dfe0bdccdef8ff0df5f2cf5fd
Ikl0 <klimovs2008van@gmail.com>
2022-06-02 20:27:55 +0300

    storing files in local

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -5,6 +5,7 @@ class Operation < ApplicationRecord
             inclusion: { in: 5.years.ago..5.years.from_now }
   has_many :operation_details, dependent: :destroy
   belongs_to :user
+  has_one_attached :cover_picture
   scope :with_user, ->(user_id) { where('user_id = ?', user_id) }
   scope :with_amount_gteq, lambda { |sum|
                              joins('left join operation_details on operations.id = operation_details.operation_id')
