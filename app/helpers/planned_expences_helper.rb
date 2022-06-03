fcd0a40555ad3e08a6b898bf400f794adbd35035
Vitaly <wrather28@gmail.com>
2022-06-03 17:14:46 +0300

    add method to planned_expence_helper fot get user.email

--- app/helpers/planned_expences_helper.rb
+++ app/helpers/planned_expences_helper.rb
@@ -4,4 +4,8 @@ module PlannedExpencesHelper
   def planned_expence_name(expence_id)
     expence = Expence.find(expence_id).name
   end
+
+  def send_email(user_id)
+    user = User.find(user_id).email
+  end
 end
