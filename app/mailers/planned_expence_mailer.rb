0e0cee5aacba111040ceb12956f43dba293e50e7
Vitaly <wrather28@gmail.com>
2022-06-03 17:11:39 +0300

    create planned_expence_mailer

---
+++ app/mailers/planned_expence_mailer.rb
@@ -0,0 +1,6 @@
+class PlannedExpenceMailer < ApplicationMailer
+	def notification_about_expence
+		@user = params[:user]
+		mail to: @user.email, subject: 'Notification about planned expence | FinanceTracker'
+	end
+end
