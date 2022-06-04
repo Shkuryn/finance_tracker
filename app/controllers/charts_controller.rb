1020af692da71b0f154b264da707188968e9ebf2
igor shkuryn <shkuryn@gmail.com>
2022-06-04 22:30:46 +0400

    add earnings and  expences on dashboard

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -11,7 +11,7 @@ class ChartsController < ApplicationController
     @balance = 998.76
     @balance_percent = '25'
     @spent_current_month = spent_current_month
-    @incomes_current_month = 1540
+    @incomes_current_month = incomes_current_month
     @expences_chart_data = OperationDetail.joins('INNER JOIN expences on expences.id =operation_details.expence_id')
                            .joins(:operation).where(operation: { user_id: current_user.id })
                            .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month)
@@ -31,7 +31,14 @@ class ChartsController < ApplicationController
   end
   def spent_current_month
     OperationDetail.joins(:operation).where(operation: { user_id: current_user.id})
+      .where(operation: { operation_type: 0})
       .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month)
+                   .sum(:amount)
+  end
+  def incomes_current_month
+    OperationDetail.joins(:operation).where(operation: { user_id: current_user.id})
+                   .where(operation: { operation_type: 1})
+                   .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month)
                    .sum(:amount)
   end
   def planned_current_month
