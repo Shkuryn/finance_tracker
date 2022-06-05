2c08ad6f2d66a7be9c73115c580b8e7e3c7ffe3f
vitaly <wrather28@gmail.com>
2022-06-05 16:08:59 +0300

    create migration and changed planned_expence#create for integration with delayed_job

--- app/controllers/planned_expences_controller.rb
+++ app/controllers/planned_expences_controller.rb
@@ -27,7 +27,7 @@ class PlannedExpencesController < ApplicationController
 
     respond_to do |format|
       if @planned_expence.save
-        PlannedExpenceMailer.with(user: @user).notification_about_expence.deliver_now
+        PlannedExpenceMailer.delay(run_at: @planned_expence.date).notification_about_expence
         format.html { redirect_to planned_expence_url(@planned_expence), notice: "Planned Expence was successfully created." }
         format.json { render :show, status: :created, location: @planned_expence }
       else
