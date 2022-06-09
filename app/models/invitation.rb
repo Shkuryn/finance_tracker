9fd8c2e47f4892387e32300ddba88e83875d1422
Vitaly <wrather28@gmail.com>
2022-06-09 10:13:15 +0300

    create self.confirmed_record? method in invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -6,5 +6,12 @@ class Invitation < ApplicationRecord
     case2 = !Invitation.where(user_id: id2, member_id: id1).empty?
     case1 || case2
   end
+
+  def self.confirmed_record?(id1, id2)
+    case1 = !Invitation.where(user_id: id1, member_id: id2, confirmed: true).empty?
+    case2 = !Invitation.where(user_id: id2, member_id: id1, confirmed: true).empty?
+    case1 || case2
+  end
+
   
 end
