8c91f1479839dbb90024c280134adfcd5261bd1d
Vitaly <wrather28@gmail.com>
2022-06-09 10:32:29 +0300

    implement Invitations#create

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,10 @@
 class InvitationsController < ApplicationController
+    def create
+        id1 = params[:ids][:id1]
+        id2 = params[:ids][:id2]
+        @invitation = Invitation.new(user_id: id1, member_id: id2)
+        @invitation.save
+        redirect_to current_user
+    end
+
 end
