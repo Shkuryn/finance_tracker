f145e8a90c13ac6122da11bc9b09615b8f5c15f7
Vitaly <wrather28@gmail.com>
2022-06-09 10:11:01 +0300

    create self.reacted? method in invitation model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,3 +1,10 @@
 class Invitation < ApplicationRecord
   belongs_to :user
+
+  def self.reacted?(id1, id2)
+    case1 = !Invitation.where(user_id: id1, member_id: id2).empty?
+    case2 = !Invitation.where(user_id: id2, member_id: id1).empty?
+    case1 || case2
+  end
+  
 end
