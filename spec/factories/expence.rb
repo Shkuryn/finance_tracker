4d369f88db096f4c4e8634d58700e756889e75b9
Ikl0 <klimovs2008van@gmail.com>
2022-06-12 19:19:34 +0300

    rewrite factory for expence

--- spec/factories/expence.rb
+++ spec/factories/expence.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :expences do
+  factory :expence do
     id { '1' }
     name { 'Great name' }
     description { 'Good description' }
