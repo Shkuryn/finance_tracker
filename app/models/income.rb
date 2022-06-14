feb0acd0a4d873b78fb2ffb5cfce5f270754f49d
Ikl0 <klimovs2008van@gmail.com>
2022-06-14 14:16:14 +0300

    add validation for income

--- app/models/income.rb
+++ app/models/income.rb
@@ -2,6 +2,7 @@
 
 class Income < ApplicationRecord
   belongs_to :user
+  validates :name, length: { minimum: 3, maximum: 50 }, allow_blank: false
   self.primary_key = 'id'
   has_many :operation_details, dependent: :delete_all
   scope :with_user, ->(user_id) { where('user_id = ?', user_id) }
