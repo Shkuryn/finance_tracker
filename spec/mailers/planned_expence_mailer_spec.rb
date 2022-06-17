4dd00defc8d44d1c71f8b23213bcc33bca56a238
Vitaly <wrather28@gmail.com>
2022-06-17 15:48:17 +0300

    add config for mailer to test env and add spec for pla_exp_mailer

---
+++ spec/mailers/planned_expence_mailer_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PlannedExpenceMailer, :type => :mailer do
+  describe "#notification_about_expence" do
+    let(:user) { FactoryBot.create :user }
+    let(:mail) { PlannedExpenceMailer.notification_about_expence(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq('Notification about planned expence | FinanceTracker')
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(['dreamteamc2022@gmail.com'])
+    end
+
+    it "renders the body" do
+        expect(mail.body.encoded).to match("Notification about your planned expence! Plese visit this URL for show more details")
+    end
+  end
+end
