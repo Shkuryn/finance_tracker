731bb044841c24028b2d58346b10045836797d9e
igor shkuryn <shkuryn@gmail.com>
2022-06-18 11:16:06 +0400

    apply filter with family members

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -4,8 +4,9 @@ class ChartsController < ApplicationController
   before_action :check_user_signed, only: %i[show new edit update destroy index]
 
   def index
-    @members = User.with_family(current_user.family_id)
-                   .to_a.reject { |u| u == current_user }.pluck(:name)
+    # @members = User.with_family(current_user.family_id)
+    #                .to_a.reject { |u| u == current_user }.pluck(:name)
+    @members = User.with_family(current_user.family_id).pluck(:name)
   end
 
   def create
@@ -18,20 +19,22 @@ class ChartsController < ApplicationController
     @end_date3 = params_hash['/charts?method=post'].fetch('end_date(3i)')
     @object_for_analyze = params_hash['/charts?method=post'].fetch('object_for_analyze')
     @type_of_chart = params_hash['/charts?method=post'].fetch('type_of_chart')
-    # @start_date = params_hash['/charts?method=post'].fetch('start_date')
     @start_date = Date.civil(@start_date1.to_i, @start_date2.to_i, @start_date3.to_i)
-    # @end_date = params_hash['/charts?method=post'].fetch('end_date')
     @end_date = Date.civil(@end_date1.to_i, @end_date2.to_i, @end_date3.to_i)
+    @by_members = params_hash['/charts?method=post'].fetch('by_members')
+
     @chart_data = if @object_for_analyze == 'Expences'
                     OperationDetail.joins('INNER JOIN expences on expences.id =operation_details.expence_id')
-                                   .joins(:operation).where(operation: { user_id: current_user.id })
+                                   .joins(:operation)
+                                   .where(operation: { user_id: members_id })
                                    .where('date BETWEEN ? AND ?',
                                           @start_date.present? ? @start_date : DateTime.new(2022, 1, 1, 0, 0, 0), @end_date.present? ? @end_date : DateTime.new(2100, 1, 1, 0, 0, 0))
                                    .group(:name).sum(:amount)
                                    .sort_by { |_key, value| value }.reverse.to_h
                   else
                     OperationDetail.joins('INNER JOIN incomes on incomes.id =operation_details.income_id')
-                                   .joins(:operation).where(operation: { user_id: current_user.id })
+                                   .joins(:operation)
+                                   .where(operation: { user_id: members_id })
                                    .where('date BETWEEN ? AND ?',
                                           @start_date.present? ? @start_date : DateTime.new(2022, 1, 1, 0, 0, 0), @end_date.present? ? @end_date : DateTime.new(2100, 1, 1, 0, 0, 0))
                                    .group(:name).sum(:amount)
@@ -74,5 +77,14 @@ class ChartsController < ApplicationController
                      .where(operation: { operation_type: 0 })
                      .where('date  <= ?', Date.current.end_of_day)
                      .sum(:amount)
+  end
+
+  def members_id
+    if @by_members.count > 1
+      User.with_family(current_user.family_id).where('name IN (?)', @by_members)
+    else
+      User.where('id = ?', current_user.id)
+
+    end
   end
 end
