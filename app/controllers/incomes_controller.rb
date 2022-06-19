7002d394b0807d02e8b6e0228a0d3b4efaa3a910
igor shkuryn <shkuryn@gmail.com>
2022-06-19 22:13:01 +0400

    user can use others family memebers incomes

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -6,7 +6,7 @@ class IncomesController < ApplicationController
 
   # GET /incomes
   def index
-    @q = Income.where(predefined: true).or(Income.with_user(current_user.id)).ransack(params[:q]) unless current_user.nil?
+    @q = Income.where(predefined: true).or(Income.with_family(current_user.family_id)).ransack(params[:q]) unless current_user.nil?
     @incomes = @q.result(distinct: true)
   end
 
