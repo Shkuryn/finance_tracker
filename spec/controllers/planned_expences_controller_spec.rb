6bd9cf22778e6395061fc716fe26ff936a619e40
Vitaly <wrather28@gmail.com>
2022-06-20 14:33:33 +0300

    #update planned-expences-controller_spec

--- spec/controllers/planned_expences_controller_spec.rb
+++ spec/controllers/planned_expences_controller_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe PlannedExpencesController, type: :controller do
   let(:planned_expence) { FactoryBot.create(:planned_expence, user_id: user.id, amount: 10, expence_id: expence.id, id: 1) }
   let(:user2) { FactoryBot.create(:user, id: 2, name: 'Dmitry', surname: 'Usik', email: 'test@example.com') }
   let(:planned_expence2) { FactoryBot.create(:planned_expence2, user_id: user2.id, id: 2, amount: 20, expence_id: expence.id) }
+
   describe '#index' do
     it 'returns status 200 OK' do
       get :index
@@ -24,10 +25,10 @@ RSpec.describe PlannedExpencesController, type: :controller do
     end
     it 'returns only current user planned expences' do
       login_user user
-      planned_expences = PlannedExpence.with_user(user.id)
-      expect(planned_expences).to eq(planned_expence)
+      expect(planned_expence.user_id).to eq(user.id)
     end
   end
+
   describe '#show' do
     it 'has a related heading when not signed in' do
         get :index
@@ -35,12 +36,51 @@ RSpec.describe PlannedExpencesController, type: :controller do
         assert_template('welcome/index')
     end
   end
+
   describe '#create' do
-    subject { FactoryBot.create(:planned_expence, user_id: user.id) }
     it 'shoud create a new planned expence' do
       login_user user
       visit new_planned_expence_path
       expect(page).to have_content('New Planned Expence') 
     end
+    it 'creates an class instance' do
+      expect { planned_expence }.to change { PlannedExpence.count }.by(1)
+    end
+    it 'registered user can add new planned expence' do
+      expence3 = FactoryBot.create(:expence, user_id: user.id, predefined: true, name: 'test')
+      login_user user
+      visit new_planned_expence_path
+      select expence3.name, from: "planned_expence[expence_id]"
+      fill_in "Description", with: 'test description'
+      fill_in 'amount', with: 20
+      click_on 'save'
+      expect(response).to have_http_status(:ok)
+      expect(planned_expence.user_id)to eq(user.id)
+      expect(planned_expence.sent)to eq(false)
+    end
+  end
+
+  describe '#destroy' do
+    it 'deletes item' do
+      expect do
+        planned_expence.destroy
+      end.to change(PlannedExpence, :count).by(-1)
+    end
+  end
+
+  describe '#update' do
+    context "with good data" do
+      it "updates planned_expence and redirects" do
+        patch :update, id: @planned_expence.id, planned_expence: { description: "test_description_updated", amount: 100}
+        expect(response).to be_redirect
+      end
+    end
+    context "with bad data" do
+      it "does not change planned_expence, and re-renders the form" do
+        patch :update, id: @planned_expence.id, planned_expence: { description: "test_description_updated", amount: 'bad_value'}
+        expect(response).not_to be_redirect
+      end
+    end
+
   end
 end
