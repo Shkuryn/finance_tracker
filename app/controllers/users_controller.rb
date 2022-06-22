3cff80d162ee73625f3d3b728ad897ea36172dc9
Vitaly <wrather28@gmail.com>
2022-06-22 12:45:54 +0300

    fix users vision

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
+  before_action :check_user_signed
+
   def show
     @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_url, alert: "Sorry, This Profile belongs to someone else !"
+    end
     @invitation = Invitation.new
   end
 
@@ -11,3 +17,14 @@ class UsersController < ApplicationController
     @users = @users.emails(params[:email]) if params[:email].present?
   end
 end
+
+
+  private
+
+  def check_user_signed
+    render template: 'welcome/index' unless user_signed_in?
+  end
+
+  def user_params
+    params.require(:user).permit(:id)
+  end
