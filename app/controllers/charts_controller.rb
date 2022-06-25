052aadf741b15feb68d0bc94c596fa94c70d2d19
igor shkuryn <shkuryn@gmail.com>
2022-06-25 11:12:23 +0400

    remove unused methods

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -51,36 +51,6 @@ class ChartsController < ApplicationController
     render template: 'welcome/index' unless user_signed_in?
   end
 
-  def spent_current_month
-    OperationDetail.joins(:operation).where(operation: { user_id: current_user.id })
-                   .where(operation: { operation_type: 0 })
-                   .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month)
-                   .sum(:amount)
-  end
-
-  def incomes_current_month
-    OperationDetail.joins(:operation).where(operation: { user_id: current_user.id })
-                   .where(operation: { operation_type: 1 })
-                   .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month)
-                   .sum(:amount)
-  end
-
-  def planned_current_month
-    PlannedExpence.with_user(current_user.id)
-                  .where('date BETWEEN ? AND ?', Date.current.beginning_of_month, Date.current.end_of_month).sum(:amount)
-  end
-
-  def balance
-    OperationDetail.joins(:operation).where(operation: { user_id: current_user.id })
-                   .where(operation: { operation_type: 1 })
-                   .where('date  <= ?', Date.current.end_of_day)
-                   .sum(:amount) -
-      OperationDetail.joins(:operation).where(operation: { user_id: current_user.id })
-                     .where(operation: { operation_type: 0 })
-                     .where('date  <= ?', Date.current.end_of_day)
-                     .sum(:amount)
-  end
-
   def members_id
     if @by_members.count > 1
       User.with_family(current_user.family_id).where('name IN (?)', @by_members)
